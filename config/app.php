49f40111464cdbdf7d7660c9cb610bd9329228e6
chuckprocash <e.procashchakraborty@gmail.com>
2025-11-12 14:12:23 +0600

    correction modified app.php

--- config/app.php
+++ config/app.php
@@ -52,8 +52,8 @@ return [
     |
     */
 
-    // 'url' => env('APP_URL', 'http://localhost'),
-    'url' => env('APP_URL', 'https://clinicare-zl04.onrender.com'),
+    'url' => env('APP_URL', 'http://localhost'),
+    // 'url' => env('APP_URL', 'https://clinicare-zl04.onrender.com'),
 
     /*
     |--------------------------------------------------------------------------
