45cd4d8bcef7d649153971e8de282176535ca183
chuckprocash <e.procashchakraborty@gmail.com>
2025-11-12 13:31:11 +0600

    modified appServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,9 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
-        }
          // Share data with ALL views that use app layout
         View::composer('admin.template', function ($view) {
             $view->with([
